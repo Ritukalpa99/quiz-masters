01b859f5f0a3483a92598300f7d867a3b772182f
Ritukalpa Gogoi <ritukalpa.gogoi99@gmail.com>
2022-04-14 14:35:16 +0530

    file structure setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useGlobalContext } from "./Context";
+import SetupForm from "./SetupForm";
+import Loading from "./Loading";
+import Modal from "./Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const {waiting,loading, questions, index, correct} = useGlobalContext()
+  if(waiting) {
+    return <SetupForm/>
+  }
+  if(loading) {
+    return <Loading/>
+  }
+  
+  const {question, incorrect_answers, correct_answers} = questions[0]
+  const answers = [...incorrect_answers,correct_answers]
+  return <main>
+    <Modal/>
+  </main>
 }
 
-export default App;
+export default App
---
+++ src/SetupForm.js
@@ -0,0 +1,7 @@
+import { useGlobalContext } from "./Context";
+
+const SetupForm = () => {
+    return <h2>setup form</h2>
+}
+
+export default SetupForm
